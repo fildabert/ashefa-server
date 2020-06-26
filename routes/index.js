8413f0cc9ee531405d9deb3addb8aa5f9809293b
fildabert <filberttt@hotmail.com>
2020-06-26 11:23:32 +0700

    add index routing

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const userRoute = require('./user-route');
 const sessionRoute = require('./session-route');
+const locationRoute = require('./location-route');
 
 const router = express.Router();
 
 router.use('/users', userRoute);
+router.use('/locations', locationRoute);
 router.use('/sessions', sessionRoute);
 
 module.exports = router;
